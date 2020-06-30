2a629f16c52961d0b5d86facc6dc5a7aa06fe3a5
Frantz Paul <fpaul787@gmail.com>
2020-06-30 10:27:39 -0400

    Updates

---
+++ precache-manifest.ae4c5361c0ac4819e3ca2c3934cf6b37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01bf3989ee74a20c42781085cd93d5e7",
+    "url": "/React-Weather/index.html"
+  },
+  {
+    "revision": "2cd7ece3559a754467ac",
+    "url": "/React-Weather/static/css/main.1cabf206.chunk.css"
+  },
+  {
+    "revision": "4a79708fe2bb8d98f2f2",
+    "url": "/React-Weather/static/js/2.07144284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather/static/js/2.07144284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd7ece3559a754467ac",
+    "url": "/React-Weather/static/js/main.6f89110e.chunk.js"
+  },
+  {
+    "revision": "0c4561f6f6e3632b0d84",
+    "url": "/React-Weather/static/js/runtime-main.782ab875.js"
+  },
+  {
+    "revision": "c4048225327a8fc0981bfa06e1d59180",
+    "url": "/React-Weather/static/media/cold-weather.c4048225.jpg"
+  },
+  {
+    "revision": "fce2bdd002f2f2ab4ed961a824451a71",
+    "url": "/React-Weather/static/media/warm-weather.fce2bdd0.jpg"
+  }
+]);
